214a757ff7ea552c369d505d659cc959fc60c6eb
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-09-14 17:41:51 +0300

    setup settings

---
+++ src/snake_game/settings.py
@@ -0,0 +1,12 @@
+GRID_WIDTH: int = 20
+GRID_HIGHT: int = 20
+
+
+CELL_SIZE: int = 20
+
+FPS: int = 10
+
+# colors (R, G, B)
+COLOR_BG = (0, 0, 0)
+COLOR_SNAKE = (0, 255, 0)
+COLOR_FOOD = (255, 0, 0)
